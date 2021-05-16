a40179ae0324029da2613d920f9df1304e735255
James Armbrust <james.armbrust@gmail.com>
2021-05-16 14:15:51 -0600

    image and some cleanup

--- src/NavbarMain/index.jsx
+++ src/NavbarMain/index.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Image from 'react-bootstrap/Image'
 import { GoMarkGithub } from 'react-icons/go';
 import { GrLinkedin } from 'react-icons/gr';
+import avatar from './../images/jamesavatar2.png';
 
 export const NavbarMain = () => {
 
+  const renderTooltip = props => (
+    <Tooltip id="tooltip-right" {...props}>
+      A rather inccurate avatar of me...
+    </Tooltip>
+  );
+
   return(
     <>
+      <div className="avatar-frame">
+        <OverlayTrigger
+          key="right"
+          placement="right"
+          overlay={renderTooltip}
+          className="avatar-frame"
+        >
+          <Image className="avatar-circle" src={avatar} alt="avatar" roundedCircle fluid />
+        </OverlayTrigger>
+      </div>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Navbar.Brand href="/">James Armbrust</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
